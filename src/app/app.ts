d733e2ed5cd23de7070f6a4c483891db42594ccf
YusufProject660 <darajat.app@gmail.com>
2025-11-17 21:16:33 +0400

    Prepared component folders and world map folder for direct use"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WorldMapComponent } from './components/world-map/world-map.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WorldMapComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
